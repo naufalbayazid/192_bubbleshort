0d594e33f1af11275c160767ae8f861246745d23
naufalbayazid <127088739+naufalbayazid@users.noreply.github.com>
2023-03-17 18:16:55 +0700

    Add files via upload

---
+++ Bubblesort.cpp
@@ -0,0 +1,84 @@
+// Bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+ 
+void input() {
+	while (true) {
+		cout << "masukkan banyaknya elemen pada array: ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "masukkan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+} 
+
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "element array yang telah tersusun" << endl;
+	cout << "====================" << endl;
+		for (int j = 0; j < n; j++) {
+			cout << a[j] << endl;
+		}
+	cout << endl;
+} 
+
+void bubblesortarray() {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	input();
+	bubblesortarray();
+	display();
+	return 0;
+}
+
+
+
+	
+
+
+
+
+
+	 
+
+		
+		
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
